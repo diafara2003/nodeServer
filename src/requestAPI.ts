b12a36ff54af1679256f680147fa62585785c19b
Cristian lozano <yatogami2814@gmail.com>
2024-04-24 10:40:23 -0500

    ✨  #5 se implementa la refactorizacion en el componente de proveedores

--- src/requestAPI.ts
+++ src/requestAPI.ts
@@ -1,70 +1,64 @@
 import axios from "axios";
 import { envs } from "./config/envs";
 
-export async function requestAPI(request:any, token:string) {
+export async function requestAPI(request: any, token: string) {
+  if (request.AllowAnonymous == null) request.AllowAnonymous = false;
+  if (request.isformData == null) request.isformData = false;
 
-    if (request.AllowAnonymous == null) request.AllowAnonymous = false;
-    if (request.isformData == null) request.isformData = false;
+  const _http = axios.create({
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
 
+      Authorization: `Bearer ${token}`,
+    },
+    baseURL: envs.URL__BASE_API,
+  });
 
-    const _http = axios.create({
-        headers: {
-            Accept: "application/json",
-            'Content-Type': 'application/json',
+  let response = null;
 
-            Authorization: `Bearer ${token}`
-        },
-        baseURL: envs.URL__BASE_API
-    });
+  try {
+    switch (request.type) {
+      case "GET":
+        response = await _http.get(request.metodo);
+        break;
 
-    let response = null;
+      case "DELETE":
+        response = await _http.delete(request.metodo);
+        break;
 
-    try {
-        switch (request.type) {
-
-            case "GET":
-                response = await _http.get(request.metodo);
-                break;
-
-            case "DELETE":
-                response = await _http.delete(request.metodo);
-                break;
-
-            case "POST":
-
-                if (request.data != null) {
-
-                    if (request.isformData)
-                        response = await _http.post(request.metodo, request.data);
-                    else
-                        response = await _http.post(request.metodo, JSON.stringify(request.data));
-                }
-                break;
-
-            case "PUT":
-
-                if (request.data != null) {
-
-                    if (request.isformData)
-                        response = await _http.put(request.metodo, request.data);
-                    else
-                        response = await _http.put(request.metodo, JSON.stringify(request.data));
-                }
-                break;
-
+      case "POST":
+        if (request.data != null) {
+          if (request.isformData)
+            response = await _http.post(request.metodo, request.data);
+          else
+            response = await _http.post(
+              request.metodo,
+              JSON.stringify(request.data)
+            );
         }
-    } catch (error) {
-        console.log(error);
-        response = null;
+        break;
+
+      case "PUT":
+        if (request.data != null) {
+          if (request.isformData)
+            response = await _http.put(request.metodo, request.data);
+          else
+            response = await _http.put(
+              request.metodo,
+              JSON.stringify(request.data)
+            );
+        }
+        break;
     }
+  } catch (error) {
+    console.log(error);
+    response = { data: error };
+  }
 
-
-    if (response != null) {
-        return response.data;
-
-    } else {
-        return ({});
-    }
-
-
+  if (response != null) {
+    return response.data;
+  } else {
+    return {};
+  }
 }
